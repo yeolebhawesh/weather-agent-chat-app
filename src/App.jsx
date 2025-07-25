0c4785a32615f74357e41cdb5e54e21a914b9b53
Bhawesh Yeole <bhaweshyeole1808@gmail.com>
2025-07-25 15:48:59 +0530

    Button Changes

--- src/App.jsx
+++ src/App.jsx
@@ -146,10 +146,11 @@ const App = () => {
           onKeyDown={handleKeyPress}
           placeholder="Type your weather query..."
         />
-       <button type="submit">
+       <button type="submit" onClick={handleSend}>
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="20"
+
     height="20"
     viewBox="0 0 24 24"
     fill="none"
